0aa81c9ce0086517f77c474c3057791e893efc0b
OnichiEiEi <mongri2244@gmail.com>
2025-03-01 01:48:15 +0700

    Update configmb.js

--- mbproject/configmb.js
+++ mbproject/configmb.js
@@ -6,7 +6,6 @@ function calenderShow() {
     document.getElementById("homePage").classList.remove("visible");
     document.getElementById("calendarPage").classList.remove("hidden");
     document.getElementById("calendarPage").classList.add("visible");
-    sendToAppInventor("calenderShow");
 }
 
 function homeShow() {
@@ -15,17 +14,14 @@ function homeShow() {
     document.getElementById("calendarPage").classList.remove("visible");
     document.getElementById("homePage").classList.remove("hidden");
     document.getElementById("homePage").classList.add("visible");
-    sendToAppInventor("homeShow");
 }
 
 function showMood() {
     document.getElementById('emojiOverlay').style.display = 'flex';
-    sendToAppInventor("showMood");
 }
 
 function closeOverlay() {
     document.getElementById('emojiOverlay').style.display = 'none';
-    sendToAppInventor("closeOverlay");
 }
 
 function selectEmoji(emoji) {
@@ -37,7 +33,6 @@ function selectEmoji(emoji) {
     }
     updateMonthEmoji(emoji);
     closeOverlay(); // Close the overlay after selecting an emoji
-    sendToAppInventor("selectEmoji");
 }
 
 function updateMonthEmoji(emoji) {
@@ -181,10 +176,3 @@ const emojiMessages = {
         "Keep exploring"
     ]
 };
-
-// Function to send message to App Inventor
-function sendToAppInventor(message) {
-    if (window.AppInventor) {
-        window.AppInventor.setWebViewString(message);
-    }
-}
